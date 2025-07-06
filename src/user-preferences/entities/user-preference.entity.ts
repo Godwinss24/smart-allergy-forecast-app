e2e24e13052c8a142ad28a4a59730af9f677e009
Godwinss24 <godwinsudoinyang@gmail.com>
2025-07-06 16:37:23 +0100

    feat(user-preference): define AllergenType enum and update entity fields
    - Added AllergenType enum with values: TREE, GRASS, WEED
    - Updated UserPreference entity to include:
      - lat and lng as float coordinates
      - sensitiveTo field as enum of AllergenType
      - timeToAlert as a timestamp

--- src/user-preferences/entities/user-preference.entity.ts
+++ src/user-preferences/entities/user-preference.entity.ts
@@ -1,1 +1,21 @@
-export class UserPreference {}
+import { AllergenType } from "src/shared/enums/AllergenType";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserPreference {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ nullable: false, type: "float" })
+    lat: string;
+
+    @Column({ nullable: false, type: "float" })
+    lng: string;
+
+    @Column({ nullable: false, type: 'enum', enum: AllergenType })
+    sensitiveTo: AllergenType;
+
+    @Column({ nullable: false, type: 'timestamp' })
+    timeToAlert: Date;
+}
